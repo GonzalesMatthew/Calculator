a2d7ddea8dcb91e662ea01e55758dec60fbefda4
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-07-31 12:57:26 -0500

    Made a new user input to accept user request on the array. Made switch case to perform either multiplication or square of array.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,41 +9,54 @@ namespace Calculator
             Console.WriteLine("Plese enter comma separated integers: e.g. 2,4,5");
             string userInput = Console.ReadLine();
 
+            Console.WriteLine("Do you want to multiply these together or return the array of squares? (Enter '*' or '^'.)");
+            var request = Console.ReadLine();
+
+
             // convert string of numbers to string array
             string[] stringArray = userInput.Split(',', StringSplitOptions.RemoveEmptyEntries);
             // declare int array
             int[] numbers = new int[stringArray.Length];
             // convert string array to int array
-            for(int i =0; i < stringArray.Length; i ++)
+            for (int i = 0; i < stringArray.Length; i++)
             {
                 numbers[i] = int.Parse(stringArray[i]);
             }
 
-            // multiple the int array elements together:
-            int product = numbers[0];
-
-            for(int i =1; i < numbers.Length; i ++)
-            {
-                product = product * numbers[i];
-            }
-
-            Console.WriteLine($"The product of your submitted values is {product}.");
-
-            // square each element of the int array, then build a new int array with the squared values:
-            // first, create new array to store squared values in
-            int[] squaredArray = new int[numbers.Length];
-
-            // next, calculate the square of each element and push them into squaredArray
-            for(int i =0; i < numbers.Length; i ++)
-            {
-                int square = numbers[i] * numbers[i];
-                squaredArray[i] = square;
-                Console.Write("{0}, ", square);
-            }
-            Console.WriteLine();
-            Console.WriteLine($"Here are your original values squared.");
-
             
+
+            switch (request.ToLower())
+            {
+                case "*":
+                    // multiple the int array elements together:
+                    int product = numbers[0];
+
+                    for (int i = 1; i < numbers.Length; i++)
+                    {
+                        product = product * numbers[i];
+                    }
+                    Console.WriteLine($"The product of your submitted values is {product}.");
+                    break;
+                case "^":
+                    // square each element of the int array, then build a new int array with the squared values:
+                    // first, create new array to store squared values in
+                    int[] squaredArray = new int[numbers.Length];
+
+                    // next, calculate the square of each element and push them into squaredArray
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        int square = numbers[i] * numbers[i];
+                        squaredArray[i] = square;
+                        Console.Write("{0}, ", square);
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine($"Here are your original values squared.");
+                    break;
+                default:
+                    Console.WriteLine("Sorry, not a valid request.");
+                    break;
+            }
+
         }
     }
 }
